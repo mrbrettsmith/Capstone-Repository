0166f3dc0b849899926165a8008a73d3e9d735a1
mrbrettsmith <94158551+mrbrettsmith@users.noreply.github.com>
2024-11-08 21:10:55 -0600

    starting to figure out relative positions

--- Natural Neighborhood Network/main.js
+++ Natural Neighborhood Network/main.js
@@ -8,6 +8,28 @@ let stickyPosition = stickyItem.offsetTop;
 let fixItem = document.getElementById('stickyPuzzlePiece');
 let fixPosition = fixItem.offsetTop;
 
+let navElement = document.getElementById("stickyNavBar");
+console.log("NavBar Width: " + navElement.offsetWidth + "px");
+console.log("Navbar Height: " + navElement.offsetHeight + "px");
+
+let puzzleElement = document.getElementById("stickyPuzzlePiece");
+console.log("Puzzle Width: " + puzzleElement.offsetWidth + "px");
+console.log("Puzzle Height: " + puzzleElement.offsetHeight + "px");
+
+window.onresize = function(){ 
+    navElement = document.getElementById("stickyNavBar");
+    console.log("NavBar Width: " + navElement.offsetWidth + "px");
+    console.log("Navbar Height: " + navElement.offsetHeight + "px");
+
+    puzzleElement = document.getElementById("stickyPuzzlePiece");
+    console.log("Puzzle Width: " + puzzleElement.offsetWidth + "px");
+    console.log("Puzzle Height: " + puzzleElement.offsetHeight + "px");
+}
+
+var count = 0;
+console.log(count); 
+
+
 function addOrRemoveSticky() {
     if (window.scrollY >= stickyPosition) {
         stickyItem.classList.add('sticky');
@@ -22,6 +44,9 @@ function addOrRemoveFix() {
     } else {
         fixItem.classList.remove('puzzleFixed');
     }
+
+    
+
 }
 
 window.onscroll = () => {
