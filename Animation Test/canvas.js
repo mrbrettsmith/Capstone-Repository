06b260449cbcc6012ba7d739c2dcdf68f6ca21bd
mrbrettsmith <94158551+mrbrettsmith@users.noreply.github.com>
2024-11-19 10:41:06 -0600

    Canvas and animations might work OK

--- Animation Test/canvas.js
+++ Animation Test/canvas.js
@@ -4,37 +4,39 @@ window.addEventListener('DOMContentLoaded', (event) => {
     var ctx = canvas.getContext('2d');
     const frame = document.getElementById('graphic');
   
-
     function resizeCanvas() {
-        canvas.width = frame.innerWidth;
-        canvas.height = frame.innerHeight;
+        canvas.width = frame.offsetWidth;
+        canvas.height = frame.offsetHeight;
     
-        // Redraw your canvas content here if necessary
-    }
-    console.log(canvas);
-
-    resizeCanvas();
-    window.addEventListener('resize', resizeCanvas); 
-
-  
-
-    function puzz(options) {
-        this.x = options.x;
-        this.y = options.y;
-        this.width = 100;
-        this.height = 100;
-        this.color = red;
-    }
-
-    function drawPuzz(puzz) {
-        ctx.fillStyle = puzz.color;
-        ctx.fillRect = (puzz.x, puzz,y, puzz.width, puzz.height);
-    }
-
+        // build this with a better function, but draw works now, and scales
     for(var i = 0; i < 5; i++) {
         var x = 100;
-        var y = 300 * i;
-        ctx.fillRect(x,y, 100,100);
+        var y = i * 125;
+        ctx.fillRect( x, y, 100, 100);
     }
+
+        // Redraw your canvas content here if necessary
+    }
+    
+    // Initial resize on load
+    resizeCanvas();
+    // Resize the canvas when the window is resized
+    window.addEventListener('resize', resizeCanvas); 
+  
+
+    // function puzz(options) {
+    //     this.x = options.x;
+    //     this.y = options.y;
+    //     this.width = 50;
+    //     this.height = 50;
+    //     this.color = red;
+    // }
+
+    // function drawPuzz(puzz) {
+    //     ctx.fillStyle = puzz.color;
+    //     ctx.fillRect = (puzz.x, puzz,y, puzz.width, puzz.height);
+    // }
+
+
 
 });
