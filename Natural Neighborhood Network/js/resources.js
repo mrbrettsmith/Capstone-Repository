5730c63bc1546614867fcdfede2598f0f2f4169f
mrbrettsmith <94158551+mrbrettsmith@users.noreply.github.com>
2024-12-01 14:33:58 -0600

    secondary pages are finally responsive

--- Natural Neighborhood Network/js/resources.js
+++ Natural Neighborhood Network/js/resources.js
@@ -17,12 +17,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     window.addEventListener('scroll', () => {
         addOrRemoveSticky();
+
     });
     
     // MAKE NAV STICKY
     let stickyItem = document.getElementById('stickyNavBar');
     let stickyPosition = stickyItem.offsetTop;
     
+    //REFRESH OFFSET ON WINDOW RESIZE - BUSTED
+    // function newNavOffsetHeight() {
+    //     const stickyItem = document.getElementById('stickyNavBar'); 
+    //     const stickyPosition = stickyItem.offsetTop; 
+    //     return stickyPosition; 
+    //   }
+    //   window.addEventListener("resize", newNavOffsetHeight); 
+    //   let stickyPosition = newNavOffsetHeight();
+
     function addOrRemoveSticky() {
         if (window.scrollY >= stickyPosition) {
             stickyItem.classList.add('sticky');
